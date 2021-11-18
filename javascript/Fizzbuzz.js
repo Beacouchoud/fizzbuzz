8e5417a40da612504a36e1f05ebe6f0a73d09bf7
Bea <34965770+Beacouchoud@users.noreply.github.com>
2021-11-18 12:51:12 +0100

    Step 1 with the tests passed

--- javascript/Fizzbuzz.js
+++ javascript/Fizzbuzz.js
@@ -1,3 +1,29 @@
-const Fizzbuzz = (number) => '1'
+const isFizz = (number) => {
+    return number % 3 == 0
+}
+
+const isBuzz = (number) => {
+    return number % 5 == 0
+}
+
+const Fizzbuzz = (number) => {
+        // Guard clause to prevent numbers not allowed
+        if (number < 1 || number > 100) throw Error('Number not allowed')
+
+        // Empty string to append
+        let result = '';
+
+        //If number is divisible by 3
+        if (isFizz(number)) result += 'Fizz';
+        
+        //If number is divisible by 3
+        if (isBuzz(number)) result += 'Buzz';
+
+        if(!result) result = number.toString();
+
+        return result;
+}
+
+
 
 module.exports = Fizzbuzz;
--- javascript/Fizzbuzz.test.js
+++ javascript/Fizzbuzz.test.js
@@ -7,22 +7,23 @@ describe('Fizzbuzz', () => {
     expect(result).toBe('1');
   });
   
-  it.skip('returns Fizz if the number is divisible by 3', () => {
+  it('returns Fizz if the number is divisible by 3', () => {
     const result = Fizzbuzz(6);
+
     expect(result).toBe('Fizz');
   });
 
-  it.skip('returns Buzz if the number is divisible by 5', () => {
+  it('returns Buzz if the number is divisible by 5', () => {
     const result = Fizzbuzz(10);
     expect(result).toBe('Buzz');
   });
 
-  it.skip('returns FizzBuzz if the number is divisible by both 3 and 5', () => {
+  it('returns FizzBuzz if the number is divisible by both 3 and 5', () => {
     const result = Fizzbuzz(15);
     expect(result).toBe('FizzBuzz');
   });
 
-  it.skip('returns the string representation of the number if it is not divisible neither by 3 nor 5', () => {
+  it('returns the string representation of the number if it is not divisible neither by 3 nor 5', () => {
     const result = Fizzbuzz(8);
     expect(result).toBe('8');
   });
